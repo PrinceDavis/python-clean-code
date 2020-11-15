dfb86f2766355700a8bac46affcf2a9b3be63b24
Ossaija ThankGod <codebugsolved@gmail.com>
2020-11-15 10:37:04 +0100

    test postregss repo

--- clean/tests/conftest.py
+++ clean/tests/conftest.py
@@ -7,3 +7,13 @@ from clean.app import create_app
 @pytest.yield_fixture(scope='function')
 def app():
     return create_app(TestConfig)
+
+
+def pytest_addoption(parser):
+    parser.addoption("--integration", action="store_true",
+                     help="run integration tests")
+
+
+def pytest_runtest_setup(item):
+    if 'integration' in item.keywords and not item.config.getvalue("integration"):
+        pytest.skip("need --integration option to run")
